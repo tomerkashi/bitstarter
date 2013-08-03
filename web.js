515cc3b2a9b23fbd1c5c80cfa037645b026ed217
tomerkashi <tomerkashi@gmail.com>
2013-08-03 17:06:45 +0000

    pics are added

--- web.js
+++ web.js
@@ -4,8 +4,21 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  console.log("got request")
   var buffer  = fs.readFileSync("index.html")
   response.send(buffer.toString());
+});
+
+app.get('/keyboard.jpg', function(request, response) {
+  console.log("got key")
+  var buffer  = fs.readFileSync("keyboard.jpg")
+  response.send(buffer);
+});
+
+app.get('/fast_signup.jpg', function(request, response) {
+  console.log("got key")
+  var buffer  = fs.readFileSync("fast_signup.jpg")
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 8080;
